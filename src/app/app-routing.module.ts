2c79057646fab2346cf2df0973c3c6bcc5085836
sumbes <p10475242@amtsybexad.onmicrosft.com>
2020-05-22 23:42:30 +0530

    childroute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,42 @@ import { DashbordComponent } from "./dashbord/dashbord.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { ErrorComponent } from "./error/error.component";
+import { StudentComponent } from "./student/student.component";
+import { StudentDetailsComponent } from "./student-details/student-details.component";
+import { StudentragistrationComponent } from "./studentragistration/studentragistration.component";
+import { StudentcontactdetailsComponent } from "./studentcontactdetails/studentcontactdetails.component";
+import { StudentparantdetailsComponent } from "./studentparantdetails/studentparantdetails.component";
 
 const routes: Routes = [
-  { path:'', redirectTo: 'dashbord', pathMatch: "full" },
+  { path: "", redirectTo: "dashbord", pathMatch: "full" },
   { path: "dashbord", component: DashbordComponent },
   { path: "about", component: AboutComponent },
   { path: "about/:id", component: AboutComponent },
   { path: "contact", component: ContactComponent },
-  { path: "**", component: ErrorComponent }
+
+  // -- 2class********* bad practice*********************
+  // { path: "student", component: StudentComponent },
+  // { path: "student/studentdetails", component: StudentDetailsComponent },
+  // { path: "student/studentragistration", component: StudentragistrationComponent },
+
+  //childranroot
+  {
+    path: "student",
+    children: [
+      { path: "", component: StudentComponent, pathMatch: "full" },
+      {
+        path: "details",
+        children: [
+          { path: "", component: StudentDetailsComponent, pathMatch: "full" },
+          { path: "contact", component: StudentcontactdetailsComponent },
+          { path: "parant", component: StudentparantdetailsComponent },
+        ],
+      },
+      { path: "ragistration", component: StudentragistrationComponent },
+    ],
+  },
+
+  // { path: "**", component: ErrorComponent }
 ];
 
 @NgModule({
